4d7867a4e4e99ad4ca453585726d4ca53e825f95
juan garassino <juan.garassino@hotmail.com>
2022-06-14 20:36:15 +0200

    update

--- deepSculpt/plotter.py
+++ deepSculpt/plotter.py
@@ -1,19 +1,35 @@
+# from xml.dom import NO_MODIFICATION_ALLOWED_ERR
 import matplotlib.pyplot as plt
 import numpy as np
 from deepSculpt.sculptor import Sculptor
+from datetime import datetime
 
 class Plotter(Sculptor):
 
-    def __init__(self, void, style):
-        self.void = void
+    def __init__(self,
+                 volumes,
+                 colors,
+                 figsize=25,
+                 style="#ffffff",
+                 dpi=100,
+                 transparent=False):
+
+        self.void = volumes
+        self.volumes = volumes
+        self.colors = colors
+        self.figsize = figsize
         self.style = style
+        self.dpi = dpi
+        self.transparent = transparent
 
     def plot_sections(self):
         sculpture = self.void
         fig, axes = plt.subplots(ncols=6,
                                  nrows=int(np.ceil(self.void.shape[0] / 6)),
-                                 figsize=(25, 25),
-                                 facecolor=(self.style))
+                                 figsize=(self.figsize, self.figsize),
+                                 facecolor=(self.style),
+                                 dpi=self.dpi)
+
         axes = axes.ravel()  # flats
         for index in range(self.void.shape[0]):
             axes[index].imshow(sculpture[index, :, :], cmap="gray")
@@ -21,29 +37,35 @@ class Plotter(Sculptor):
     def plot_sculpture(self):  # add call to generative sculpt and then plot like 12
         fig, axes = plt.subplots(ncols=2,
                                  nrows=2,
-                                 figsize=(25, 25),
+                                 figsize=(self.figsize, self.figsize),
                                  facecolor=(self.style),
-                                 subplot_kw=dict(projection="3d"))
+                                 subplot_kw=dict(projection="3d"),
+                                 dpi=self.dpi)
+
         axes = axes.ravel()  # flats
-        for index in range(1):
-            axes[0].voxels(self.void[0],
-                               facecolors=self.void[1],
-                               edgecolors="k",
-                               linewidth=0.05)
 
-            axes[1].voxels(np.rot90(self.void[0], 1),
-                           facecolors=np.rot90(self.void[1], 1),
+        for index in range(1): # to print one color i need a condition to not rotate!!
+            axes[0].voxels(self.volumes,
+                           facecolors=self.colors,
                            edgecolors="k",
                            linewidth=0.05)
 
-            axes[2].voxels(np.rot90(self.void[0], 2),
-                           facecolors=np.rot90(self.void[1], 2),
+            axes[1].voxels(np.rot90(self.volumes, 1),
+                           facecolors=np.rot90(self.colors, 1),
                            edgecolors="k",
                            linewidth=0.05)
 
-            axes[3].voxels(np.rot90(self.void[0], 3),
-                           facecolors=np.rot90(self.void[1], 3),
+            axes[2].voxels(np.rot90(self.volumes, 2),
+                           facecolors=np.rot90(self.colors, 2),
                            edgecolors="k",
                            linewidth=0.05)
 
-        plt.savefig('image.png')  # agregar tiempo de impresion y exportar 3D
+            axes[3].voxels(np.rot90(self.volumes, 3),
+                           facecolors=np.rot90(self.colors, 3),
+                           edgecolors="k",
+                           linewidth=0.05)
+
+        now = datetime.now().strftime("%d-%m-%Y-%H-%M")
+
+        plt.savefig(f'image[{now}].png', transparent=self.transparent)  # agregar tiempo de impresion y exportar 3D
+        plt.savefig(f'image[{now}].svg', transparent=self.transparent)
