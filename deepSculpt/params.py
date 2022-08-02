0063373258f6270839e31dcb9069c4d0e0f47ac0
juan garassino <juan.garassino@hotmail.com>
2022-08-02 16:50:36 +0200

    big update

--- deepSculpt/params.py
+++ deepSculpt/params.py
@@ -1,5 +1,7 @@
 from tensorflow.random import normal
 
+LOCALLY = False
+
 CREATE_DATA = True
 
 N_SAMPLES = 5000
@@ -18,8 +20,26 @@ SCULPTS_GEN = 1
 
 SEED = normal([SCULPTS_GEN, NOISE_DIM])
 
-BUCKET_NAME = 'deepsculpt'
+BUCKET_NAME = "deepsculpt"
 
 BUCKET_TRAIN_DATA_PATH = "data"
 
 MODEL_BASE_PATH = ""
+
+"""        void_dim,
+        n_edge_elements,
+        n_plane_elements,
+        n_volume_elements,
+        color_edges,
+        color_planes,
+        color_volumes,
+        element_edge_min,
+        element_edge_max,
+        element_grid_min,
+        element_grid_max,
+        element_plane_min,
+        element_plane_max,
+        element_volume_min,
+        element_volume_max,
+        step,
+        verbose,"""
